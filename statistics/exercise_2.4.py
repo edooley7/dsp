1882bc85c2eb77a5dc6f33eefbaf1231847fd2e9
laumontemayor <laumontemayor@gmail.com>
2015-05-22 09:33:28 -0400

    added stats folder

---
+++ statistics/exercise_2.4.py
@@ -0,0 +1,6 @@
+#### 1) Think Stats Exercise 2.4
+
+# Using the variable `totalwgt_lb`, investigate whether first babies
+# are lighter or heavier than others. Compute Cohen’s d to quantify the
+# difference between the groups. How does it compare to the difference in
+# pregnancy length?
