29bc6d342bdc565d1ea5264ae52ecb1406a1aca1
Aaron Schumacher <ajschumacher@gmail.com>
2015-05-28 15:06:17 -0400

    combine, remove dup stuff, remove main()

--- python/01-strings.py
+++ python/01-strings.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python2.4 -tt
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
+#!/usr/bin/env python
 
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
+# Based on materials copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
 
 # Basic string exercises
 # Fill in the code for the functions below. main() is already set up
@@ -64,59 +61,28 @@ def mix_up(a, b):
   return
 
 
-# Provided simple test() function used in main() to print
-# what each function returns vs. what it's supposed to return.
-def test(got, expected):
-  if got == expected:
-    prefix = ' OK '
-  else:
-    prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+print 'donuts'
+# Each line calls donuts, compares its result to the expected for that call.
+test(donuts(4), 'Number of donuts: 4')
+test(donuts(9), 'Number of donuts: 9')
+test(donuts(10), 'Number of donuts: many')
+test(donuts(99), 'Number of donuts: many')
+print 'both_ends'
+test(both_ends('spring'), 'spng')
+test(both_ends('Hello'), 'Helo')
+test(both_ends('a'), '')
+test(both_ends('xyz'), 'xyyz')
+print 'fix_start'
+test(fix_start('babble'), 'ba**le')
+test(fix_start('aardvark'), 'a*rdv*rk')
+test(fix_start('google'), 'goo*le')
+test(fix_start('donut'), 'donut')
+print 'mix_up'
+test(mix_up('mix', 'pod'), 'pox mid')
+test(mix_up('dog', 'dinner'), 'dig donner')
+test(mix_up('gnash', 'sport'), 'spash gnort')
+test(mix_up('pezzy', 'firm'), 'fizzy perm')
 
-
-# Provided main() calls the above functions with interesting inputs,
-# using test() to check if each result is correct or not.
-def main():
-  print 'donuts'
-  # Each line calls donuts, compares its result to the expected for that call.
-  test(donuts(4), 'Number of donuts: 4')
-  test(donuts(9), 'Number of donuts: 9')
-  test(donuts(10), 'Number of donuts: many')
-  test(donuts(99), 'Number of donuts: many')
-
-  print
-  print 'both_ends'
-  test(both_ends('spring'), 'spng')
-  test(both_ends('Hello'), 'Helo')
-  test(both_ends('a'), '')
-  test(both_ends('xyz'), 'xyyz')
-
-  
-  print
-  print 'fix_start'
-  test(fix_start('babble'), 'ba**le')
-  test(fix_start('aardvark'), 'a*rdv*rk')
-  test(fix_start('google'), 'goo*le')
-  test(fix_start('donut'), 'donut')
-
-  print
-  print 'mix_up'
-  test(mix_up('mix', 'pod'), 'pox mid')
-  test(mix_up('dog', 'dinner'), 'dig donner')
-  test(mix_up('gnash', 'sport'), 'spash gnort')
-  test(mix_up('pezzy', 'firm'), 'fizzy perm')
-
-
-# Standard boilerplate to call the main() function.
-if __name__ == '__main__':
-  main()
-#!/usr/bin/python2.4 -tt
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
 
 # Additional basic string exercises
 
@@ -157,36 +123,16 @@ def front_back(a, b):
   return
 
 
-# Simple provided test() function used in main() to print
-# what each function returns vs. what it's supposed to return.
-def test(got, expected):
-  if got == expected:
-    prefix = ' OK '
-  else:
-    prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
-
-
-# main() calls the above functions with interesting inputs,
-# using the above test() to check if the result is correct or not.
-def main():
-  print 'verbing'
-  test(verbing('hail'), 'hailing')
-  test(verbing('swiming'), 'swimingly')
-  test(verbing('do'), 'do')
-
-  print
-  print 'not_bad'
-  test(not_bad('This movie is not so bad'), 'This movie is good')
-  test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
-  test(not_bad('This tea is not hot'), 'This tea is not hot')
-  test(not_bad("It's bad yet not"), "It's bad yet not")
-
-  print
-  print 'front_back'
-  test(front_back('abcd', 'xy'), 'abxcdy')
-  test(front_back('abcde', 'xyz'), 'abcxydez')
-  test(front_back('Kitten', 'Donut'), 'KitDontenut')
-
-if __name__ == '__main__':
-  main()
+print 'verbing'
+test(verbing('hail'), 'hailing')
+test(verbing('swiming'), 'swimingly')
+test(verbing('do'), 'do')
+print 'not_bad'
+test(not_bad('This movie is not so bad'), 'This movie is good')
+test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+test(not_bad('This tea is not hot'), 'This tea is not hot')
+test(not_bad("It's bad yet not"), "It's bad yet not")
+print 'front_back'
+test(front_back('abcd', 'xy'), 'abxcdy')
+test(front_back('abcde', 'xyz'), 'abcxydez')
+test(front_back('Kitten', 'Donut'), 'KitDontenut')
