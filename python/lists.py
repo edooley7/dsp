ce9885c7965970ba4d1682a04aa311d2bdb29c70
edooley7 <edooley7@gmail.com>
2015-06-21 00:38:04 -0400

    Update lists.py

--- python/lists.py
+++ python/lists.py
@@ -3,86 +3,109 @@
 
 
 def match_ends(words):
-    """
-    Given a list of strings, return the count of the number of strings
-    where the string length is 2 or more and the first and last chars
-    of the string are the same.
-
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
-    """
-    raise NotImplementedError
+	count = 0
+	
+	for i in words:
+		if len(i) >= 2:
+			if i[0] == i[-1]:
+				count += 1
+			else:
+				count
+		else:
+			count
+	
+	print count
+	
+match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
+match_ends(['', 'x', 'xy', 'xyx', 'xx'])				
+match_ends(['aaa', 'be', 'abc', 'hello'])			
 
 
 def front_x(words):
-    """
-    Given a list of strings, return a list with the strings in sorted
-    order, except group all the strings that begin with 'x' first.
-    e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
-         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
+	x_words = []
+	not_x_words = []
+	
+	for i in words:
+		if i[0] == "x":
+			x_words.append(i)
+		else:
+			not_x_words.append(i)
+	
+	new_list = []
+	
+	for i in sorted(x_words):
+		new_list.append(i)
+	for i in sorted(not_x_words):
+		new_list.append(i)
+		
+	print new_list 
+	
+front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
+front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
+front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
 
-    >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
-    ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
-    >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
-    ['xaa', 'xcc', 'aaa', 'bbb', 'ccc']
-    >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
-    ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
-    """
-    raise NotImplementedError
+def middle_of_list(listed):
+	if len(listed) > 2:
+		return listed[1]
+	else: 
+		return None
 
 
 def sort_last(tuples):
-    """
-    Given a list of non-empty tuples, return a list sorted in
-    increasing order by the last element in each tuple.
-    e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
-         [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
+	switched_list = []
+	listed = map(list, tuples)
+	
+	for i in range(0,len(listed)):
+		first = listed[i][0]
+		last = listed[i][-1]
+		middle = listed[i][1]
+		if len(listed[i]) == 2:
+			switched = last, first
+			switched_list.append(list(switched))
+		else:
+			switched = last, middle, first
+			switched_list.append(list(switched))
+	switched_sorted_list = sorted(switched_list)
+	
 
-    >>> sort_last([(1, 3), (3, 2), (2, 1)])
-    [(2, 1), (3, 2), (1, 3)]
-    >>> sort_last([(2, 3), (1, 2), (3, 1)])
-    [(3, 1), (1, 2), (2, 3)]
-    >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
-    [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
-    """
-    raise NotImplementedError
+	final_list = []
+	
+	for i in range(0, len(switched_sorted_list)):
+		first_new = switched_sorted_list[i][0]
+		last_new = switched_sorted_list[i][-1]
+		middle_new = middle_of_list(switched_sorted_list[i])
+		if middle_new != None:
+			switched_back = last_new, middle_new, first_new
+		else:
+			switched_back = last_new, first_new
+		final_list.append(list(switched_back))
+	
+	print final_list
+
+sort_last([(1, 3), (3, 2), (2, 1)])
+sort_last([(2, 3), (1, 2), (3, 1)])
+sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
 
 
 def remove_adjacent(nums):
-    """
-    Given a list of numbers, return a list where all adjacent equal
-    elements have been reduced to a single element, so [1, 2, 2, 3]
-    returns [1, 2, 3]. You may create a new list or modify the passed
-    in list.
-
-    >>> remove_adjacent([1, 2, 2, 3])
-    [1, 2, 3]
-    >>> remove_adjacent([2, 2, 3, 3, 3])
-    [2, 3]
-    >>> remove_adjacent([3, 2, 3, 3, 3])
-    [3, 2, 3]
-    >>> remove_adjacent([])
-    []
-    """
-    raise NotImplementedError
+  new_list = []
+  for i in nums:
+    if new_list:
+      if new_list[-1] != i:
+        new_list.append(i)
+    else: new_list.append(i)        
+  print new_list
+  
+remove_adjacent([1, 2, 2, 3])
+remove_adjacent([2, 2, 3, 3, 3])
+remove_adjacent([3, 2, 3, 3, 3])
+remove_adjacent([])
 
 
 def linear_merge(list1, list2):
-    """
-    Given two lists sorted in increasing order, create and return a
-    merged list of all the elements in sorted order. You may modify
-    the passed in lists. Ideally, the solution should work in "linear"
-    time, making a single pass of both lists.
+	list1.extend(list2)
+	print sorted(list1)
 
-    >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
-    ['aa', 'aa', 'aa', 'bb', 'bb']
-    """
-    raise NotImplementedError
+linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
+linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
+linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
